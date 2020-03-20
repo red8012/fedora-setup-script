170921ae55f080a1e0b06d24ffad5e2ef7eb1dbd
Yu-Ann Chen <red8012@gmail.com>
2020-03-20 20:43:00 +0800

    NEW: install nano and set as default editor

--- run.sh
+++ run.sh
@@ -23,7 +23,7 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 read -p "Install software [Enter]"
-sudo dnf install nodejs yarnpkg zsh bat fzf grubby gnome-tweaks lsd youtube-dl zopfli aria2 tldr dconf-editor lollypop filezilla gitg audacity seahorse code transmission make nosync thefuck util-linux-user
+sudo dnf install nodejs yarnpkg zsh bat fzf grubby gnome-tweaks lsd youtube-dl zopfli aria2 tldr dconf-editor lollypop filezilla gitg audacity seahorse code transmission make nosync thefuck util-linux-user nano
 
 read -p "Set keyboard.dispatch in VSCode to keyCode"
 
--- zshrc
+++ zshrc
@@ -4,7 +4,9 @@ export LC_ALL=en_US.UTF-8
 ANTIBODY_HOME="$(antibody home)"
 export ZSH="$ANTIBODY_HOME"/https-COLON--SLASH--SLASH-github.com-SLASH-robbyrussell-SLASH-oh-my-zsh
 source ~/.zsh_plugins.sh  # start antibody
+
 eval $(thefuck --alias)
+export EDITOR=nano
 
 update_antibody() {
     antibody bundle < ~/.zsh_plugins.txt > ~/.zsh_plugins.sh
